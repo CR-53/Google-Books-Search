71eb105bc32790363cabac72a1db7ac3707f2377
Chris Roschi <chrisroschi53@gmail.com>
2020-10-06 14:26:34 +1030

    added search form

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navigation from "./components/Navigation";
+import Search from "./components/Search"
 
 function App() {
   return (
     <Router>
       <div>
         <Navigation />
+        <Search />
       </div>
     </Router>
   );
 }
-
 
 export default App;
---
+++ client/src/components/Search/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Search() {
+    return (
+        <div className="container search-box">
+            <div className="row">
+                <div className="col-md-12">
+                    <h3 className="section-heading">Book Search</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            {/* <Form.Label>Book</Form.Label> */}
+                            <Form.Control type="text" placeholder="Search for a book" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
