a56efc482f97bd36de0792cd33f86b720c52a041
Chris Roschi <chrisroschi53@gmail.com>
2020-10-06 13:18:14 +1030

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Navigation />
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
